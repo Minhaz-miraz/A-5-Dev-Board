fad3db1dc60d360e644b861eaf9857d4d51284d8
Minhaz-miraz <minhajulmiraz28@gmail.com>
2025-03-01 15:42:04 +0600

    button all disabled

--- script/completeButton.js
+++ script/completeButton.js
@@ -1,68 +1,105 @@
-let totalDone = document.getElementById('total-task-done');
-let totalDoneCvt = parseInt(document.getElementById('total-task-done').innerText);
+// let totalDone = document.getElementById('total-task-done');
+// let totalDoneCvt = parseInt(document.getElementById('total-task-done').innerText);
 
-let taskAssigned = document.getElementById('task-assigned');
-let taskAssignedCvt = parseInt(document.getElementById('task-assigned').innerText);
+// let taskAssigned = document.getElementById('task-assigned');
+// let taskAssignedCvt = parseInt(document.getElementById('task-assigned').innerText);
 
-document.getElementById('btn1')
-    .addEventListener('click', function () {
-        alert('Board Updated Successfully');
-        const taskAdded = totalDoneCvt + 1;
-        totalDoneCvt = taskAdded;
-        totalDone.innerText = taskAdded.toString();
+// document.getElementById('btn1')
+//     .addEventListener('click', function () {
+//         alert('Board Updated Successfully');
+//         const taskAdded = totalDoneCvt + 1;
+//         totalDoneCvt = taskAdded;
+//         totalDone.innerText = taskAdded.toString();
 
-        const taskReduced = taskAssignedCvt - 1;
-        taskAssignedCvt = taskReduced;
-        taskAssigned.innerText = taskAssignedCvt;
-
-
-
-        // button disable
-        document.getElementById('btn1').disabled = true;
-        document.getElementById('btn1').style.backgroundColor = "gray";
-        document.getElementById('btn1').style.cursor = "default";
-    })
-
-document.getElementById('btn2')
-    .addEventListener('click', function () {
-        alert('Board Updated Successfully')
-        const taskAdded = totalDoneCvt + 1;
-        totalDoneCvt = taskAdded;
-        totalDone.innerText = taskAdded.toString();
-
-        const taskReduced = taskAssignedCvt - 1;
-        taskAssignedCvt = taskReduced;
-        taskAssigned.innerText = taskAssignedCvt;
+//         const taskReduced = taskAssignedCvt - 1;
+//         taskAssignedCvt = taskReduced;
+//         taskAssigned.innerText = taskAssignedCvt;
 
 
 
-        // button disable
-        document.getElementById('btn2').disabled = true;
-        document.getElementById('btn2').style.backgroundColor = "gray";
-        document.getElementById('btn2').style.cursor = "default";
+//         // button disable
+//         document.getElementById('btn1').disabled = true;
+//         document.getElementById('btn1').style.backgroundColor = "gray";
+//         document.getElementById('btn1').style.cursor = "default";
+//     })
 
-    })
-document.getElementById('btn3')
-    .addEventListener('click', function () {
-        alert('Board Updated Successfully')
+// document.getElementById('btn2')
+//     .addEventListener('click', function () {
+//         alert('Board Updated Successfully')
+//         const taskAdded = totalDoneCvt + 1;
+//         totalDoneCvt = taskAdded;
+//         totalDone.innerText = taskAdded.toString();
 
-        const taskAdded = totalDoneCvt + 1;
-        totalDoneCvt = taskAdded;
-        totalDone.innerText = taskAdded.toString();
-
-        const taskReduced = taskAssignedCvt - 1;
-        taskAssignedCvt = taskReduced;
-        taskAssigned.innerText = taskAssignedCvt;
-
+//         const taskReduced = taskAssignedCvt - 1;
+//         taskAssignedCvt = taskReduced;
+//         taskAssigned.innerText = taskAssignedCvt;
 
 
-        // button disable
-        document.getElementById('btn3').disabled = true;
-        document.getElementById('btn3').style.backgroundColor = "gray";
-        document.getElementById('btn3').style.cursor = "default";
+
+//         // button disable
+//         document.getElementById('btn2').disabled = true;
+//         document.getElementById('btn2').style.backgroundColor = "gray";
+//         document.getElementById('btn2').style.cursor = "default";
+
+//     })
+// document.getElementById('btn3')
+//     .addEventListener('click', function () {
+//         alert('Board Updated Successfully')
+
+//         const taskAdded = totalDoneCvt + 1;
+//         totalDoneCvt = taskAdded;
+//         totalDone.innerText = taskAdded.toString();
+
+//         const taskReduced = taskAssignedCvt - 1;
+//         taskAssignedCvt = taskReduced;
+//         taskAssigned.innerText = taskAssignedCvt;
 
 
-    })
+
+//         // button disable
+//         document.getElementById('btn3').disabled = true;
+//         document.getElementById('btn3').style.backgroundColor = "gray";
+//         document.getElementById('btn3').style.cursor = "default";
+
+
+//     })
+
+
+// button-update-stats
+
+document.getElementById('btn1').addEventListener('click', function () {
+    // function-in-utilities
+    buttonEvent(document.getElementById('btn1'));
+    buttonDisable(document.getElementById('btn1'));
+})
+document.getElementById('btn2').addEventListener('click', function () {
+    buttonEvent(document.getElementById('btn2'));
+    buttonDisable(document.getElementById('btn2'));
+})
+
+document.getElementById('btn3').addEventListener('click', function () {
+    // function-in-utilities
+    buttonEvent(document.getElementById('btn3'));
+    buttonDisable(document.getElementById('btn3'));
+})
+
+document.getElementById('btn4').addEventListener('click', function () {
+    // function-in-utilities
+    buttonEvent(document.getElementById('btn4'));
+    buttonDisable(document.getElementById('btn4'));
+})
+
+document.getElementById('btn5').addEventListener('click', function () {
+    // function-in-utilities
+    buttonEvent(document.getElementById('btn5'));
+    buttonDisable(document.getElementById('btn5'));
+})
+
+document.getElementById('btn6').addEventListener('click', function () {
+    // function-in-utilities
+    buttonEvent(document.getElementById('btn6'));
+    buttonDisable(document.getElementById('btn6'));
+})
 
 
 
--- script/utilities.js
+++ script/utilities.js
@@ -1,4 +1,68 @@
-function taskStatus (button){
-    return 'hi';
+function buttonEvent(button) {
+    let totalDone = document.getElementById('total-task-done');
+    let totalDoneCvt = parseInt(document.getElementById('total-task-done').innerText);
 
+    let taskAssigned = document.getElementById('task-assigned');
+    let taskAssignedCvt = parseInt(document.getElementById('task-assigned').innerText);
+    alert('Board Updated Successfully');
+    const taskAdded = totalDoneCvt + 1;
+    totalDoneCvt = taskAdded;
+    totalDone.innerText = taskAdded.toString();
+
+    const taskReduced = taskAssignedCvt - 1;
+    taskAssignedCvt = taskReduced;
+    taskAssigned.innerText = taskAssignedCvt;
+
+
+
+
+    return button;
 }
+
+
+function buttonDisable(disable) {
+    if (disable == document.getElementById('btn1')) {
+
+        document.getElementById('btn1').disabled = true;
+        document.getElementById('btn1').style.backgroundColor = "gray";
+        document.getElementById('btn1').style.cursor = "default";
+        return disable;
+    }
+    if (disable == document.getElementById('btn2')) {
+
+        document.getElementById('btn2').disabled = true;
+        document.getElementById('btn2').style.backgroundColor = "gray";
+        document.getElementById('btn2').style.cursor = "default";
+        return disable;
+    }
+    if (disable == document.getElementById('btn3')) {
+
+
+        document.getElementById('btn3').disabled = true;
+        document.getElementById('btn3').style.backgroundColor = "gray";
+        document.getElementById('btn3').style.cursor = "default";
+        return disable;
+    }
+    if (disable == document.getElementById('btn4')) {
+
+        document.getElementById('btn4').disabled = true;
+        document.getElementById('btn4').style.backgroundColor = "gray";
+        document.getElementById('btn4').style.cursor = "default";
+        return disable;
+    }
+    if (disable == document.getElementById('btn5')) {
+
+        document.getElementById('btn5').disabled = true;
+        document.getElementById('btn5').style.backgroundColor = "gray";
+        document.getElementById('btn5').style.cursor = "default";
+        return disable;
+    }
+    if (disable == document.getElementById('btn6')) {
+
+        document.getElementById('btn6').disabled = true;
+        document.getElementById('btn6').style.backgroundColor = "gray";
+        document.getElementById('btn6').style.cursor = "default";
+        return disable;
+    }
+}
+
