40a40158f5ee46f53af06f320944a1d7bfe02f35
Minhaz-miraz <minhajulmiraz28@gmail.com>
2025-03-02 16:38:21 +0600

    color added

---
+++ script/color.js
@@ -0,0 +1,14 @@
+document.getElementById("color-btn").addEventListener("click",
+    function () {
+        const body = document.getElementById("body");
+
+        const letters = "0123456789ABCDEF";
+        let color = "#";
+
+        for (let i = 0; i < 6; i++) {
+            color += letters[Math.floor(Math.random() * 16)];
+        }
+
+        body.style.backgroundColor = color;
+    }
+)
